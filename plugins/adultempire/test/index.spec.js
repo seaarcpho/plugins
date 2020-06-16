c7cfc8536f1f6d244802eaa80ad48702555f4a3a
boio <xorofficial@gmail.com>
2020-06-16 13:09:27 +0200

    fix date

--- plugins/adultempire/test/index.spec.js
+++ plugins/adultempire/test/index.spec.js
@@ -19,7 +19,11 @@ describe("adultempire", () => {
       .equal(
         "TUSHY.com is proud to present its newest studio TUSHY RAW. TUSHY RAW is straight to the point - hard, hot, and intense anal sex with the most beautiful women on the planet. Everything you see here is 100% real & raw. See more at TUSHYRAW.com"
       );
-    expect(result.releaseDate).to.be.a("number").equal(1566943200000);
+    expect(result.releaseDate).to.be.a("number");
+    const date = new Date(result.releaseDate);
+    expect(date.getDate(), 28);
+    expect(date.getMonth(), 7);
+    expect(date.getFullYear(), 2019);
   });
 
   it("Should not fetch covers etc (404)", async () => {
