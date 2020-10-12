f790005f6b9b657298604b6a40da2f5e2dfc80d3
Christopher Caroni <christophercaroni97@gmail.com>
2020-10-12 15:52:20 +0200

    chore(check_info): ignore types folder

--- check_info.js
+++ check_info.js
@@ -2,10 +2,16 @@ const fs = require("fs");
 const path = require("path");
 
 const pluginsFolder = path.resolve("plugins");
+const IGNORE_PATHS = [path.resolve("plugins/types")];
 
 for (const name of fs.readdirSync(pluginsFolder)) {
   const infoPath = path.resolve("plugins", name, "info.json");
-  if (!fs.existsSync(infoPath)) process.exit(1);
+  if (
+    !fs.existsSync(infoPath) &&
+    !IGNORE_PATHS.find((ignorePath) => infoPath.includes(ignorePath))
+  ) {
+    process.exit(1);
+  }
   console.log(`${name} OK`);
 }
 process.exit(0);
