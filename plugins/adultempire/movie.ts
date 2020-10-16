3813c419eb60bded01356593cec482cb7fae66a3
Christopher Caroni <christophercaroni97@gmail.com>
2020-10-16 15:35:01 +0200

    fix(adultempire): movie name

--- plugins/adultempire/movie.ts
+++ plugins/adultempire/movie.ts
@@ -47,7 +47,7 @@ export default async function (ctx: MyContext): Promise<MovieOutput> {
       .text()
       .replace(/[\t\n]+/g, " ")
       .replace(/ {2,}/, " ")
-      .replace("- On Sale! Porn Video Sale", "")
+      .replace(/- On Sale!.*/, "")
       .trim();
 
     $(".col-sm-4.m-b-2 li").each(function (i, elm) {
