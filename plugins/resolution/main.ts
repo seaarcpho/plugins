62d75f3bb9db33b6181507ff214a40c7ad96b487
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2020-10-23 01:00:42 +0200

    add resolution plugin v0.0.1

---
+++ plugins/resolution/main.ts
@@ -0,0 +1,49 @@
+import { SceneContext } from "../../types/scene";
+
+interface MyContext {
+  args: {
+    resolutions?: unknown;
+  };
+}
+
+module.exports = ({ args, $throw, data, scenePath, scene }: SceneContext & MyContext) => {
+  if (!scenePath) {
+    return $throw("Uh oh. You shouldn't use the plugin for this type of event");
+  }
+
+  let resolutions = [144, 240, 360, 480, 720, 1080, 2160];
+  if (args.resolutions) {
+    if (!Array.isArray(args.resolutions) || args.resolutions.some((x) => isNaN(parseInt(x)))) {
+      return $throw("Invalid resolutions array");
+    }
+    resolutions = args.resolutions.map((x) => parseInt(x));
+  }
+
+  if (scene.meta.dimensions && scene.meta.dimensions.height) {
+    const formattedResolution = `${scene.meta.dimensions.height}p`;
+    if (data.labels) {
+      return {
+        labels: [...data.labels, formattedResolution],
+      };
+    }
+    return {
+      labels: [formattedResolution],
+    };
+  }
+
+  const resolution = resolutions.find((res) => scenePath.toLowerCase().includes(`${res}p`));
+
+  if (resolution) {
+    const formattedResolution = `${resolution}p`;
+    if (data.labels) {
+      return {
+        labels: [...data.labels, formattedResolution],
+      };
+    }
+    return {
+      labels: [formattedResolution],
+    };
+  }
+
+  return {};
+};
