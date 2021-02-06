856b3ba8e0e5531553c0978ab0920c8b89f83c37
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2021-02-06 18:48:50 +0100

    adjust eslint config, close #109

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,11 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "standard",
+    /* "plugin:prettier/recommended",
+    "plugin:mocha/recommended", */
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
-    "plugin:mocha/recommended",
   ],
   globals: {
     Atomics: "readonly",
@@ -17,14 +20,20 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 11,
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
-  plugins: ["mocha", "chai-friendly"],
+  plugins: ["@typescript-eslint" /*"mocha", "chai-friendly"*/],
   rules: {
-    curly: "warn",
-    "mocha/no-mocha-arrows": "off",
+    "@typescript-eslint/require-await": "warn",
+    "no-unused-vars": "off",
+    "prefer-template": "warn",
+    curly: "error",
+    /* "mocha/no-mocha-arrows": "off", */
     // Disable the default rule
     "no-unused-expressions": 0,
     // Use the chai friendly rule
-    "chai-friendly/no-unused-expressions": 2,
+    /* "chai-friendly/no-unused-expressions": 2, */
   },
 };
