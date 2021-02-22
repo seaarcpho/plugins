17bea9f44a6ffaeeea48eaef5a479e0276368503
arcadianCdr <77160044+arcadianCdr@users.noreply.github.com>
2021-02-22 10:52:02 +0100

    Less verbose logging

--- plugins/fileparser/main.ts
+++ plugins/fileparser/main.ts
@@ -8,8 +8,6 @@ module.exports = async (ctx: MySceneContext): Promise<SceneOutput> => {
 
   if (!scenePath) $throw("Uh oh. You shouldn't use the plugin for this type of event");
 
-  $logger.verbose(`Parsing scene: ${scenePath}`);
-
   const cfg = await findAndLoadSceneConfig(ctx);
   if (!cfg) {
     $logger.warn(
@@ -20,9 +18,7 @@ module.exports = async (ctx: MySceneContext): Promise<SceneOutput> => {
   function parseReleaseDate(): Partial<{ releaseDate: number }> {
     if (args.parseDate === false) return {};
 
-    $logger.verbose("Parsing release date...");
     const dateFromName = dateToTimestamp(ctx, sceneName);
-
     if (dateFromName) {
       $logger.verbose(`Found date in scene name: ${new Date(dateFromName).toLocaleDateString()}`);
       return { releaseDate: dateFromName };
@@ -35,81 +31,67 @@ module.exports = async (ctx: MySceneContext): Promise<SceneOutput> => {
   function parseName(): Partial<{ name: string }> {
     if (!cfg || !cfg.nameMatcher) return {};
 
-    $logger.verbose("Parsing name...");
-
     const matched = matchElement(ctx, cfg.nameMatcher);
-
     if (matched) {
       $logger.verbose(`Matched name: ${matched}`);
       return { name: matched.toString().trim() };
-    } else {
-      $logger.verbose(`Found no name.`);
-      return {};
     }
+
+    $logger.verbose(`Found no name.`);
+    return {};
   }
 
   function parseStudio(): Partial<{ studio: string }> {
     if (!cfg || !cfg.studioMatcher) return {};
 
-    $logger.verbose("Parsing studio...");
-
     const matched = matchElement(ctx, cfg.studioMatcher);
-
     if (matched) {
       $logger.verbose(`Matched studio: ${matched}`);
       return { studio: matched.toString().trim() };
-    } else {
-      $logger.verbose(`Found no studio.`);
-      return {};
     }
+
+    $logger.verbose(`Found no studio.`);
+    return {};
   }
 
   function parseActors(): Partial<{ actors: string[] }> {
     if (!cfg || !cfg.actorsMatcher) return {};
 
-    $logger.verbose("Parsing actors...");
-
     const matched = matchElement(ctx, cfg.actorsMatcher);
-
     if (matched) {
       $logger.verbose(`Matched actor(s): ${matched}`);
       return { actors: matched };
-    } else {
-      $logger.verbose(`Found no actor.`);
-      return {};
     }
+
+    $logger.verbose(`Found no actor.`);
+    return {};
   }
 
   function parseMovie(): Partial<{ movie: string }> {
     if (!cfg || !cfg.movieMatcher) return {};
 
-    $logger.verbose("Parsing movie...");
-
     const matched = matchElement(ctx, cfg.movieMatcher);
-
     if (matched) {
       $logger.verbose(`Matched movie: ${matched}`);
       return { movie: matched.toString().trim() };
-    } else {
-      $logger.verbose(`Found no movie.`);
-      return {};
     }
+    
+    $logger.verbose(`Found no movie.`);
+    return {};
   }
 
   function parseLabels(): Partial<{ labels: string[] }> {
     if (!cfg || !cfg.labelsMatcher) return {};
-
-    $logger.verbose("Parsing labels...");
 
     const matched = matchElement(ctx, cfg.labelsMatcher);
 
     if (matched) {
       $logger.verbose(`Matched label(s): ${matched}`);
       return { labels: matched };
-    } else {
-      $logger.verbose(`Found no label.`);
-      return {};
     }
+
+    $logger.verbose(`Found no label.`);
+    return {};
   }
 
   const sceneOutput: SceneOutput = {
@@ -125,6 +107,11 @@ module.exports = async (ctx: MySceneContext): Promise<SceneOutput> => {
     $logger.info(`dry mode. Would have returned: ${sceneOutput}`);
     return {};
   } else {
+    if (Object.keys(sceneOutput).length) {
+      $logger.info(`Successfully matched scene: ${sceneOutput.name}`)
+    } else {
+      $logger.info(`Could not match anything for scene: ${sceneName}`)
+    }
     return sceneOutput;
   }
 };
